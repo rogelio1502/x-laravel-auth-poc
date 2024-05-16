779d3b6c168b4edae12dce16ce9b28d482caafeb
rogelio1502 <rogelio15021515@gmail.com>
2024-05-16 10:04:04 -0600

    laravel trust setup (models, migrations and admin panel)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
+use Laratrust\Contracts\LaratrustUser;
+use Laratrust\Traits\HasRolesAndPermissions;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements LaratrustUser
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRolesAndPermissions;
 
     /**
      * The attributes that are mass assignable.
